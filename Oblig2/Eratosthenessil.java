8e6094fd059a2981917723a099eb036e79450639
mrRust <hansrus@ulrik.uio.no>
2016-03-14 14:54:30 +0100

    Seq primes test working + controller are finished

--- Oblig2/Eratosthenessil.java
+++ Oblig2/Eratosthenessil.java
@@ -4,13 +4,110 @@ import java.lang.Math.*;
 
 class Main{
 	public static void main(String[] args){
-		long n = 34;
-		EratosthenesSilSequential seq = new EratosthenesSilSequential(300);
-		seq.generatePrimesByEratosthenes();
-		seq.printFact(n*n); 
+		Controller c = new Controller();
+		c.testrunPrimesSeq();
+
 	}
 }
 
+
+class Controller{
+	public void runAll(int start_len, int increment_runs, int repeat_runs, int threads){
+		
+		
+	}
+
+	public void runPrimesSeq(int start_len, int increment_runs, int repeat_runs){
+		System.out.println("Primes sequential: ");
+		repeat_runs -= (repeat_runs%2+1);
+		double timelist[] = new double[repeat_runs];
+		double time_point;
+		for (int i = 0; i < increment_runs-1; i ++){
+			for (int j = 0; j < repeat_runs; j ++){	
+				
+			}
+			
+		}
+		System.out.println();
+	}
+	public void runFactSeq(int start_Length, int increment_runs, int top, int repeat_runs){
+		System.out.println("Fact sequential:   ");
+		repeat_runs -= (repeat_runs%2+1);
+		double timelist[] = new double[repeat_runs];
+		double time_point;
+		for (int i = 0; i < increment_runs-1; i ++){
+			for (int j = 0; j < repeat_runs; j ++){	
+				
+			}
+			
+		}
+		System.out.println();
+	}
+	public void testrunPrimesSeq(){
+		System.out.println("Primes test run sequential: ");
+		EratosthenesSilSequential e = new EratosthenesSilSequential(100);
+		e.generatePrimesByEratosthenes();
+		e.printAllPrimes();
+	}
+
+	public void runPrimesPar(int start_len, int increment_runs, int repeat_runs){
+		System.out.println("Primes parallell:  ");
+		repeat_runs -= (repeat_runs%2+1);
+		double timelist[] = new double[repeat_runs];
+		double time_point;
+		for (int i = 0; i < increment_runs-1; i ++){
+			for (int j = 0; j < repeat_runs; j ++){	
+				
+				
+			}
+			
+		}
+		System.out.println();
+	}
+	public void runFactPar(int start_Length, int increment_runs, int top, int repeat_runs){
+		System.out.println("Fact parallell:    ");
+		repeat_runs -= (repeat_runs%2+1);
+		double timelist[] = new double[repeat_runs];
+		double time_point;
+		for (int i = 0; i < increment_runs-1; i ++){
+			for (int j = 0; j < repeat_runs; j ++){	
+				
+				
+			}
+			
+		}
+		System.out.println();
+	}
+	public void testrunPrimesPar(){
+		System.out.println("Primes test run parallell:  ");
+	}
+}
+class FactSequential{
+	public void printFact(long num){
+
+		ArrayList<Long> factList = factorize(num);
+		System.out.printf("%d == ", num);
+		for (int i = 0; i < factList.size()-1; i ++){
+			System.out.printf("%d * ",factList.get(i));
+		}
+		System.out.printf("%d\n", factList.get(factList.size()-1));	
+	}	
+		
+	
+
+	public ArrayList<Long> factorize(long num){
+		System.out.printf("factorize %d\n",num );
+		ArrayList<Long> factList = new ArrayList<Long>(); 
+		for (long i = 2; i <= (long)Math.sqrt(num); i++){
+			while (num % i == 0){
+				factList.add(i);
+				num /= i;
+			}
+		}
+		factList.add(num);
+		return factList;
+	}
+}
 
 
 class EratosthenesSilSequential {
@@ -20,6 +117,15 @@ class EratosthenesSilSequential {
 	EratosthenesSilSequential (int maxNum) {
 		this.maxNum = maxNum;
 		bitArr = new byte [(maxNum/16)+1];
+	} 
+	public void generatePrimesByEratosthenes() {
+		setAllBitsToOne();
+		setBitToZero(1);     
+		for (int i = 3; i <= maxNum; i ++){
+			if (getBit(i) && !isPrime(i)){
+				setMultiplumToZero(i);
+			}
+		}
 	} 
 
 	public void printAllPrimes(){
@@ -31,6 +137,7 @@ class EratosthenesSilSequential {
 		}
 		System.out.println();
 	}
+
 	public void printLast100(){
 		
 		int i = maxNum;
@@ -43,36 +150,8 @@ class EratosthenesSilSequential {
 			i --;
 		}
 	}
-	public void printFact(long num){
 
-		ArrayList<Long> factList = factorize(num);
-		System.out.printf("%d == ", num);
-		for (int i = 0; i < factList.size()-1; i ++){
-			System.out.printf("%d * ",factList.get(i));
-		}
-		System.out.printf("%d\n", factList.get(factList.size()-1));	
-		
-		
-		
-	}
-
-	public ArrayList<Long> factorize(long num){
-		System.out.printf("factorize %d\n",num );
-		ArrayList<Long> factList = new ArrayList<Long>(); 
-		for (long i = 2; i <= (long)Math.sqrt(num); i++){
-			while (num % i == 0){
-				factList.add(i);
-				num /= i;
-				
-
-			}
-		}
-		if (num != 1){
-			factList.add(num);
-		}
-		return factList;
-	}
-
+	/* ------------ Private help methods ------------ */ 
 	private void setAllBitsToOne() {
 		for (int i = 0; i < bitArr.length; i++) {
 			bitArr[i] = (byte)255;
@@ -104,21 +183,9 @@ class EratosthenesSilSequential {
 		return true;
 	}
 
-
 	private void setMultiplumToZero(int i){
 		for (int j = 1; j*i <= maxNum; j++){
 			setBitToZero(i*j);
-		}
-	} 
-	
-
-	void generatePrimesByEratosthenes() {
-		setAllBitsToOne();
-		setBitToZero(1);     
-		for (int i = 3; i <= maxNum; i ++){
-			if (getBit(i) && !isPrime(i)){
-				setMultiplumToZero(i);
-			}
 		}
 	} 
 }
